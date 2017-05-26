9aba5cee2b7d3f479c7b93555e980ba0a2cf461b
CairoCoder <cairocoder@gmail.com>
2017-05-26 18:16:35 +0200

    initial commit

--- server/srv.js
+++ server/srv.js
@@ -4,11 +4,12 @@
 const express = require('express');
 const path = require('path');
 
-const publicPath = path.join(__dirname + '../public');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.static(publicPath));
 
-
-app.listen(3000, (err) => {
-    console.log('Error!');
+app.listen(port, () => {
+    console.log(`Starting the server on port ${port}`);
 });
